7c48d8a30a2291cf1e5bbd7ee3699bde87ff829d
Warioba <ajonawarioba@gmail.com>
2025-03-13 23:34:30 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare a global variable customerName
+var customerName = "bob";
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to declare bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = "not bob"; // Implicit global variable (not recommended in real projects)
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// Declare a constant leastFavoriteCustomer in global scope
+const leastFavoriteCustomer = "someone";
+
+// Function to attempt changing leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "someone else"; // This will throw an error because constants cannot be reassigned
+}
